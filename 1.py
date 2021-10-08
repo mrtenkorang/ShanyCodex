ff0545956f0c2a9313ad4f355a31a1f2cd39f2b7
Francis Osei Annin <48407920+fransay@users.noreply.github.com>
2021-10-08 10:57:30 +0000

    Create 1.py
    Write a function to that returns the maximum integer and minimum integer, when an indefinite number of integers are inputted as integer arguments or parameters.
    Eg. maximum (1, 2,3, 4,5,6,7)
    Returns 7 as maximum and 1 as minimum
    minimum (1100,45,456)
    Returns 1100 as maximum and 45 as minimum

---
+++ 1.py
@@ -0,0 +1,23 @@
+"""Write a function to that returns the maximum integer and minimum integer, when an indefinite number of integers are inputted as integer arguments or parameters.
+Eg. maximum (1, 2,3, 4,5,6,7)
+Returns 7 as maximum and 1 as minimum
+minimum (1100,45,456)
+Returns 1100 as maximum and 45 as minimum
+"""
+
+
+def maximumAndMininum(*numbers):
+    if len(numbers)==0:
+        return None
+    else:
+        maxi = numbers[0]
+        mini = numbers[0]
+        for i in numbers[1:]:
+            if i > maxi:
+                maxi = i
+            if i < mini:
+                mini = i
+        return "maxi =",maxi,"mini =",mini
+
+if __name__ == '__main__':
+    print(maximumAndMininum(1,2,4,5,6,7,8,6543,2,3,5,6,4,3))
